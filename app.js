5402a2be8120539ce2c907a38fa6cb32eaf2fefd
Sathya Girish <sathya.prabha.girish@sap.com>
2018-07-12 10:36:00 -0400

    Adds new implementation for a declared function

--- app.js
+++ app.js
@@ -7,17 +7,30 @@ function sumOrder1(price, quantity) {
         return price * quantity;
     }
 }
-console.log("1. Total price " + sumOrder1(15, 5));
+console.log("1. Optional Parameter handling in pure js");
+console.log("Total price " + sumOrder1(15, 5));
 // console.log(`1. Total price ${sumOrder1(15)}`); - This throws an error in ts, not in js.
 //2. Optional Parameter handling in ts
 var sumOrder2 = function (price, quantity) { return price * quantity; };
-console.log("1. Total price " + sumOrder2(15, 5));
-console.log("1. Total price " + sumOrder2(15)); // returns NaN since quantity is undefined.
-//3. Declare first, define later
+console.log("2. Optional Parameter handling in ts");
+console.log("Total price " + sumOrder2(15, 5));
+console.log("Total price " + sumOrder2(15)); // returns NaN since quantity is undefined.
+//3. Syntax for function declaration that lets us change the implementation later.
+console.log("3. Syntax for function declaration that lets us change the implementation later.");
 var sumOrder3;
+//implementation 1
 sumOrder3 = function (priceVal, QtyVal) {
     if (QtyVal === void 0) { QtyVal = 1; }
     return priceVal * QtyVal;
 };
+console.log("implementation 1");
 console.log("1. Total price " + sumOrder3(15, 5));
-console.log("1. Total price " + sumOrder3(15));
+console.log("2. Total price " + sumOrder3(15));
+//implementation 2
+sumOrder3 = function (priceValue, QtyValue) {
+    if (QtyValue === void 0) { QtyValue = 0; }
+    return priceValue - QtyValue;
+};
+console.log("implementation 2");
+console.log("1. Total price " + sumOrder3(15, 5));
+console.log("2. Total price " + sumOrder3(15));
--- app.ts
+++ app.ts
@@ -7,16 +7,19 @@ function sumOrder1(price, quantity){
       }
 }
 
-console.log(`1. Total price ${sumOrder1(15, 5)}`);
+console.log("1. Optional Parameter handling in pure js");
+console.log(`Total price ${sumOrder1(15, 5)}`);
 // console.log(`1. Total price ${sumOrder1(15)}`); - This throws an error in ts, not in js.
 
 //2. Optional Parameter handling in ts
 let sumOrder2 = (price: number, quantity?:number):number=> price*quantity;
 
-console.log(`1. Total price ${sumOrder2(15, 5)}`);
-console.log(`1. Total price ${sumOrder2(15)}`); // returns NaN since quantity is undefined.
+console.log("2. Optional Parameter handling in ts");
+console.log(`Total price ${sumOrder2(15, 5)}`);
+console.log(`Total price ${sumOrder2(15)}`); // returns NaN since quantity is undefined.
 
 //3. Syntax for function declaration that lets us change the implementation later.
+console.log("3. Syntax for function declaration that lets us change the implementation later.");
 let sumOrder3: (price: number, quantity?:number)=> number;
 
 //implementation 1
@@ -24,7 +27,14 @@ sumOrder3 = (priceVal,QtyVal=1) =>{
     return priceVal*QtyVal;
 } 
 
+console.log("implementation 1");
 console.log(`1. Total price ${sumOrder3(15, 5)}`);
-console.log(`1. Total price ${sumOrder3(15)}`);
+console.log(`2. Total price ${sumOrder3(15)}`);
 
 //implementation 2
+sumOrder3 = (priceValue,QtyValue=0) => {
+    return priceValue-QtyValue;
+}
+console.log("implementation 2");
+console.log(`1. Total price ${sumOrder3(15, 5)}`);
+console.log(`2. Total price ${sumOrder3(15)}`);
